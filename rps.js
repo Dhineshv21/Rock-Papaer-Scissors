6f44aa933040985fe2d371fe33f02c5bcfc0e606
Dhineshv21 <172117441+Dhineshv21@users.noreply.github.com>
2025-02-11 22:16:14 +0530

    Remodelled Computer Choice
    Added Player Choice prompt and reworked Computer choice

--- rps.js
+++ rps.js
@@ -1,7 +1,13 @@
-function getComputerChoice(max) {
-  let Computer_choice = Math.floor(Math.random() * max);
+function getComputerChoice() {
+  return Math.floor(Math.random() * 2);
+}
 
-  switch (Computer_choice) {
+function getHumanChoice() {
+  return parseInt(prompt("What is your choice"));
+}
+
+function answer(ans) {
+  switch (ans) {
     case 0:
       return "rock";
       break;
@@ -11,7 +17,11 @@ function getComputerChoice(max) {
     case 2:
       return "scissor";
       break;
+    default:
+      return "Pick a Valid Choice";
+      break;
   }
 }
 
-
+let humanChoice = answer(getHumanChoice());
+let computerChoice = answer(getComputerChoice());
