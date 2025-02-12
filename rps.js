2c1f6ffce179aff1b485184054db46718a60c5e4
Dhineshv21 <172117441+Dhineshv21@users.noreply.github.com>
2025-02-12 10:57:59 +0530

    Added PlayRound Function
    Playround is used to check who wins

--- rps.js
+++ rps.js
@@ -15,13 +15,34 @@ function answer(ans) {
       return "paper";
       break;
     case 2:
-      return "scissor";
+      return "scissors";
       break;
     default:
       return "Pick a Valid Choice";
       break;
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "scissors") {
+    return "You Win! Rock Beats Scissors.";
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    return "You Lose! Paper Beats Rock.";
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    return "You Win! paper Beats rock";
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    return "You Lose! Scissors Beats Paper";
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    return "You Lose! Rock Beats Scissors!";
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    return "You Win! Scissors Beats paper";
+  } else {
+    return "Its a Draw";
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
 
 let humanChoice = answer(getHumanChoice());
 let computerChoice = answer(getComputerChoice());
